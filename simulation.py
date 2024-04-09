429e48aee6a6ea2d34b60d89199f67dcdffaa50a
hugoscatena <hugo.scatena@edu.ece.fr>
2024-04-09 15:08:41 +0200

    feat: add TrafficSignal and Vehicle classes _init_

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 
 # Global Constants
 FPS = 60  # Frames per second
@@ -25,3 +26,72 @@ stopLines = {'right': 480, 'down': 280, 'left': 920, 'up': 720}  # Stop lines fo
 defaultStop = {'right': 570, 'down': 360, 'left': 840, 'up': 638}  # Threshold for vehicles to cross the intersection
 noOfSignals = 4  # Number of traffic signals
 signalCords = [(572, 340), (770, 375), (795, 535), (535, 590)]  # Coordinates for signals
+
+
+class TrafficSignal:
+    """Class to represent a traffic signal."""
+
+    def __init__(self, red, yellow, green):
+        """Initialize the traffic signal."""
+        self.red = red
+        self.yellow = yellow
+        self.green = green
+        self.remaining_green_time = green
+        self.remaining_yellow_time = YELLOW_TIME
+        self.remaining_switch_time = SWITCH_DELAY
+        self.signal_status = 'green' if green > 0 else 'yellow' if yellow > 0 else 'red'
+        self.signalText = (
+            f"Green ({green})" if green > 0 else f"Yellow ({yellow})" if yellow > 0 else f"Red ({red})"
+        )
+        self.vehicles_in_front = 0
+
+
+class Vehicle(pygame.sprite.Sprite):
+    """Class to represent a vehicle."""
+
+    def __init__(self, vehicle_type, direction_number, direction, simulation_speed, simulation_instance):
+        """Initialize the vehicle."""
+        super().__init__()
+        self.simulation_instance = simulation_instance
+        self.flag = False
+        self.simulation_speed = simulation_speed
+        self.cross_time = None
+        self.spawn_time = time.time()
+        self.hit_box = None
+        self.direction_number = direction_number
+        self.vehicle_type = vehicle_type
+        self.crossed = False
+        self.acceleration = acceleration[vehicle_type]
+        self.vehicle_in_front = None
+
+        # Set the initial position based on the direction
+        if direction == 'right':
+            self.x = x['right'][0]
+            self.y = y['right'][0]
+            self.rotation = 0
+        elif direction == 'down':
+            self.x = x['down'][0]
+            self.y = y['down'][0]
+            self.rotation = 270
+        elif direction == 'left':
+            self.x = x['left'][0]
+            self.y = y['left'][0]
+            self.rotation = 180
+        elif direction == 'up':
+            self.x = x['up'][0]
+            self.y = y['up'][0]
+            self.rotation = 90
+
+        self.direction = direction
+        self.speed = speeds[vehicle_type]
+        self.stop = stopLines[direction]
+        self.defaultStop = defaultStop[direction]
+        self.colliding = False  # Flag to indicate if a collision occurred
+        self._create_hit_box()
+
+        # Add the vehicle to the appropriate lane group
+        laneGroups[self.direction].add(self)
+
+    def _create_hit_box(self):
+        self.hit_box.x = self.x
+        self.hit_box.y = self.y
